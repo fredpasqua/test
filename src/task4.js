ddc68efe6eae26bc44fc7e4b87f64f0d88d4378a
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2022-12-01 21:55:06 -0500

    filter users by fname, lname

--- src/task4.js
+++ src/task4.js
@@ -3,31 +3,48 @@ import { useState } from "react";
 import UserBox from "./userBox.jsx";
 export default function Task4() {
   const [users, setUsers] = useState([]);
+  const [query, setQuery] = useState("");
+
   const apiGET = () => {
-    fetch("https://dummyjson.com/users")
+    fetch("https://dummyjson.com/users?limit=100")
       .then((res) => res.json())
       .then((json) => setUsers(json.users));
   };
   useEffect(() => apiGET, []);
 
-  console.log(users);
+  const filteredUsers = users.filter((user) => {
+    return (
+      user.firstName.toLowerCase().includes(query.toLowerCase()) ||
+      user.lastName.toLowerCase().includes(query.toLowerCase())
+    );
+  });
+
   return (
     <>
       <div>
+        <input
+          onChange={(e) => setQuery(e.target.value)}
+          value={query}
+          placeholder="Search Names"
+        ></input>
         <div>
           <ul className="boxListItems">
-            {users.map((user) => (
-              <li key={user.id}>
-                <UserBox
-                  name={user.firstName + " " + user.lastName}
-                  image={user.image}
-                  streetAddress={user.address}
-                  manufDate={user.birthDate}
-                  title={"placeholder"}
-                  email={user.email}
-                />
-              </li>
-            ))}
+            {filteredUsers < 1 ? (
+              <h2>No known user with that name</h2>
+            ) : (
+              filteredUsers.map((user) => (
+                <li key={user.id}>
+                  <UserBox
+                    name={user.firstName + " " + user.lastName}
+                    image={user.image}
+                    streetAddress={user.address}
+                    manufDate={user.birthDate}
+                    title={"placeholder"}
+                    email={user.email}
+                  />
+                </li>
+              ))
+            )}
           </ul>
         </div>
       </div>
