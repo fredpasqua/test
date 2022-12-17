e2fea59b707c67577a44e5d84e9b78f6ec2d7630
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2022-12-17 09:15:40 -0500

    added react-modal

--- src/App.js
+++ src/App.js
@@ -6,20 +6,23 @@ import Task4 from "./task4.js";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nav from "./nav";
 function App() {
-  return (
-    <div className="App">
-      <h1 className="animate-character">Sample React.js Cards UI</h1>
 
-      <BrowserRouter>
-        <Nav />
-        <Routes>
-          <Route exact path="/" element={<Task1 />}></Route>
-          <Route path="/Task2" element={<Task2 />}></Route>
-          <Route path="/Task3" element={<Task3 />}></Route>
-          <Route path="/Task4" element={<Task4 />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
+  return (
+    <>
+      <div className="App">
+        <h1 className="animate-character">Sample React.js Cards UI</h1>
+
+        <BrowserRouter>
+          <Nav />
+          <Routes>
+            <Route exact path="/" element={<Task1 />}></Route>
+            <Route path="/Task2" element={<Task2 />}></Route>
+            <Route path="/Task3" element={<Task3 />}></Route>
+            <Route path="/Task4" element={<Task4 />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
   );
 }
 
--- src/userBox.jsx
+++ src/userBox.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+import ReactModal from "react-modal";
+import { useState } from "react";
 export default function UserBox({
   manufDate,
   streetAddress,
@@ -9,37 +10,64 @@ export default function UserBox({
   email,
 }) {
   let emailAddress = email.toString();
+  const [showModal, setShowModal] = useState(false);
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
   return (
-    <div className="infoCardUser">
-      <div className="profileBox">
-        <div
-          className="elementBox"
-          // style={{
-          //   backgroundColor: bgColor,
-          // }}
-        >
-          {image ? (
-            <img className="task3Image" src={image} alt="robot face" />
-          ) : (
-            title.toUpperCase()
-          )}
+    <>
+      <div className="infoCardUser">
+        <div className="profileBox">
+          <div
+            className="elementBox"
+            // style={{
+            //   backgroundColor: bgColor,
+            // }}
+          >
+            {image ? (
+              <img className="task3Image" src={image} alt="robot face" />
+            ) : (
+              title.toUpperCase()
+            )}
+          </div>
+          <h2 className="robotName">{name}</h2>
         </div>
-        <h2 className="robotName">{name}</h2>
-      </div>
 
-      <div className="addressGroup">
-        <h2 className="addressHeading">Address:</h2>
-        <p>{streetAddress.address}</p>
-        <br></br>
-        <p>
-          {streetAddress.city}, {streetAddress.state} {streetAddress.postalCode}
+        <div className="addressGroup">
+          <h2 className="addressHeading">Address:</h2>
+          <p>{streetAddress.address}</p>
           <br></br>
-        </p>
+          <p>
+            {streetAddress.city}, {streetAddress.state}{" "}
+            {streetAddress.postalCode}
+            <br></br>
+          </p>
+          <p className="robotEmail">
+            <a href={`mailto:${emailAddress}`}>{email}</a>
+          </p>
+        </div>
+        <button
+          className="userDetailsButton"
+          onClick={() => setShowModal(true)}
+        >
+          User Details
+        </button>
+      </div>
+      <ReactModal
+        isOpen={showModal}
+        ariaHideApp={false}
+        contentLabel="directory modal"
+      >
+        <button className="modalCloseButton" onClick={() => handleCloseModal()}>
+          X
+        </button>
+        <p>{name}</p>
         <p className="robotEmail">
           <a href={`mailto:${emailAddress}`}>{email}</a>
         </p>
-      </div>
-      <button className="userDetailsButton">User Details</button>
-    </div>
+        <h2 className="addressHeading">Address:</h2>
+        <p>{streetAddress.address}</p>
+      </ReactModal>
+    </>
   );
 }
