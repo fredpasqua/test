201b7a52915e4243c5462d937f720d2cd4ace78d
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2022-12-27 15:58:28 -0500

    updates

--- src/select.jsx
+++ src/select.jsx
@@ -1,23 +1,22 @@
 import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
+
 import Select from "react-select";
 
 export default function DropDown({ users }) {
-  let departments = users.map((user) => ({
-    value: user.company.department,
-    label: user.company.department,
-  }));
+  let departments = users
+    .map((user) => ({
+      value: user.company.department,
+      label: user.company.department,
+    }))
+    .filter(
+      (v, i, a) =>
+        a.findIndex((v2) => JSON.stringify(v2) === JSON.stringify(v)) === i
+    );
 
-  let filteredDepartments = departments.filter(
-    (v, i, a) =>
-      a.findIndex((v2) => JSON.stringify(v2) === JSON.stringify(v)) === i
-  );
-
-  console.log(filteredDepartments);
   return (
     <div className="departments">
-      <Select options={filteredDepartments} />
+      Search by department
+      <Select options={departments} />
     </div>
   );
 }
