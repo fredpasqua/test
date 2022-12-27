bbf70455213fd0d83a7d2fac4a9f73cdaa9e2c4d
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2022-12-27 15:42:33 -0500

    add filter to departments

--- src/select.jsx
+++ src/select.jsx
@@ -4,13 +4,20 @@ import { useState } from "react";
 import Select from "react-select";
 
 export default function DropDown({ users }) {
-  let departments = users.map((user) => [
-    { value: user.company.department, label: user.company.department },
-  ]);
+  let departments = users.map((user) => ({
+    value: user.company.department,
+    label: user.company.department,
+  }));
 
+  let filteredDepartments = departments.filter(
+    (v, i, a) =>
+      a.findIndex((v2) => JSON.stringify(v2) === JSON.stringify(v)) === i
+  );
+
+  console.log(filteredDepartments);
   return (
     <div className="departments">
-      <Select options={departments} />
+      <Select options={filteredDepartments} />
     </div>
   );
 }
